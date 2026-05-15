9e5606a8252cdcb9f0b1929e11805c7975542643
Preetham P <preetham2006@outlook.com>
2026-05-15 20:05:29 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (83.68%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,13 +1,20 @@
-import java.util.Arrays;
 class Solution {
     public int search(int[] nums, int target) {
-       
-        int index=Arrays.binarySearch(nums,target);
-        if(index>=0){
-            return index;
-        }
-        else {
+        int left=0;
+        int right=nums.length-1;
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(nums[mid]==target){
+                return mid;}
+                else if(nums[mid]<target){
+                    left=mid+1;
+                }
+                else{
+                    right=mid-1;
+                }
+            }
             return -1;
+            
         }
+       
     }
-}
