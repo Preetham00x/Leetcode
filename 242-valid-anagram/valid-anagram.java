f39d26db4fa22e86fda2edbd1e0735efa9ae3fa2
Preetham P <preetham2006@outlook.com>
2026-07-22 23:42:27 +0530

    Time: 7 ms (32.08%) | Memory: 46.6 MB (25.43%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,17 +1,21 @@
 import java.util.Arrays;
 class Solution {
     public boolean isAnagram(String s, String t) {
+        
         if(s.length()!=t.length()){
             return false;
         }
+        
         char[] a=s.toCharArray();
-        char[]b=t.toCharArray();
+        char[] b=t.toCharArray();
         Arrays.sort(a);
         Arrays.sort(b);
         if(Arrays.equals(a,b)){
             return true;
-        }else{
+        }
+        else{
             return false;
         }
+        
     }
 }
