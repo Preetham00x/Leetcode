f320f0aeae5b5d0fb1a882ccf4d4f82800f5808e
Preetham P <preetham2006@outlook.com>
2026-07-24 09:40:27 +0530

    Time: 14 ms (30.33%) | Memory: 46.4 MB (27.25%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean isPalindrome(String s) {
         String s1=s.toLowerCase().replaceAll("[^a-z0-9]","");
-        String rev="";
+        StringBuilder sb=new StringBuilder();
         for(int i=s1.length()-1;i>=0;i--){
-            rev=rev+s1.charAt(i);
+            sb.append(s1.charAt(i));
         }
 
-        return s1.equals(rev);
+        return s1.equals(sb.toString());
         
     }
 }
