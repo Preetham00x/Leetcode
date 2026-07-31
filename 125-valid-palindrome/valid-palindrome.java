33e3d142cf8d00f83cc673315900428f04aae54f
Preetham P <preetham2006@outlook.com>
2026-07-31 23:47:03 +0530

    Time: 136 ms (5.23%) | Memory: 48.4 MB (5.42%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,12 +1,13 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String s1=s.toLowerCase().replaceAll("[^a-z0-9]","");
-        StringBuilder sb=new StringBuilder();
-        for(int i=s1.length()-1;i>=0;i--){
-            sb.append(s1.charAt(i));
-        }
-
-        return s1.equals(sb.toString());
-        
+       String num=s.toLowerCase().replaceAll("[^a-z0-9]","");
+       String rev="";
+       for(int i=num.length()-1;i>=0;i--){
+        rev=rev+num.charAt(i);
+       }
+       if(num.equals(rev))return true;
+       else{
+        return false;
+       }
     }
 }
