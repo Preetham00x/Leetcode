5bb7090953d3863b402b2444058102427b18b361
Preetham P <preetham2006@outlook.com>
2026-08-14 01:20:57 +0530

    Time: 2 ms (91.97%) | Memory: 47.6 MB (76.69%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left=0;
+        int right=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[right]!=0){
+                int temp=nums[right];
+                nums[right]=nums[left];
+                nums[left]=temp;
+                left++;
+                
+            }
+            right++;
+        }
+        
+    }
+}
